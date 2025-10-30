e4f7eec4423cf86ed88ccb79b47d91d9937bb0a9
naseuri <93072217+naseri3@users.noreply.github.com>
2025-10-30 23:44:42 +0900

    updown game reset & btn disable coding

--- upDown-game/main.js
+++ upDown-game/main.js
@@ -22,11 +22,26 @@ let userInput = document.getElementById("userInput");
 // 결과 출력 변수
 let resultArea = document.getElementById("result-area");
 
+// 리셋 버튼
+let resetBtn = document.getElementById("resetBtn");
+
+// 남은 기회 변수
+let chances =  5;
+
+// 게임 오버 변수
+let gameOver = false;
+
+// 찬스값 출력 
+let chanceArea = document.getElementById("chance-area");
+
 // playBtn에 이벤트 추가
 // addEventListener("불러올 이벤트", 실행할 함수)
 // play를 변수로써 활용 => 함수도 매개변수로 넘길 수 있다.
 playBut.addEventListener("click", play);
+// reset 클릭 이벤트
+resetBtn.addEventListener("click", reset);
 
+// 번호 생성 함수
 function pickRandomNum() {
     computerNum = Math.floor(Math.random() * 100)+1;
     // Math.floor() : 소수점 버림 함수
@@ -36,6 +51,10 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value;
+    // 찬스 값 출력
+    chances -- ;
+    chanceArea.textContent = `남은 기회 : ${chances}번`; 
+    console.log("chances : ", chances)
     if(userValue < computerNum) {
         resultArea.textContent = "UP!!!"
     } else if(userValue > computerNum) {
@@ -43,7 +62,22 @@ function play() {
     } else {
         resultArea.textContent = "맞췄습니다!!"
     }
+
+    if(chances < 1) {
+        gameOver = true;
+    }
+    if(gameOver == true) {
+        playBut.disabled = true;
+    }
 }
 
+// 리셋 함수
+function reset() {
+    // user input창에 깨끗하게 정리되고
+    userInput.value = "";
+    // 새로운 번호가 생성되고
+    pickRandomNum();
+    resultArea.textContent = "결과값이 여기 나옵니다!";
+}
 pickRandomNum();
 
